2a94b39a3c3ab0a4adfbc7977c86c8655e0059db
Kavindutharaka <ktliyanapathirana2000@gmail.com>
2026-01-13 16:31:17 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var app = angular.module('PHVApp', []);
+app.controller('HomeCtrl', function ($scope, $interval, $http, $filter, $rootScope, $window, $timeout, $location) {
+    console.log("game runn!");
+       
+    $scope.page = 3;
+    
+});
